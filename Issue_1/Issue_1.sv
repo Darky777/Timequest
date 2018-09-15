059f803fc778c8011b9800391e21ac995754fc91
Сотников Иван <stn_iv@list.ru>
2018-09-15 16:59:39 +0300

    исправили счетчик с последовательным переносом.

--- Issue_1/Issue_1.sv
+++ Issue_1/Issue_1.sv
@@ -3,12 +3,19 @@ module Issue_1 (
     output logic led
 );
 
-logic [31:0 ] cnt;
+logic [15:0 ] cnt_low,cnt_high;
 
 always_ff @(posedge clk) begin 
-    cnt <= cnt + 1'b1;
-    led <= cnt[31];
+    cnt_low <= cnt_low + 1'b1;
 end
+always_ff @(posedge clk) begin
+    if(cnt_low == 16'hFFFF) begin
+        cnt_high <= cnt_high + 1'b1;
+    end
+    led <= cnt_high[15:0];
+end
+
+
 
 
 endmodule
