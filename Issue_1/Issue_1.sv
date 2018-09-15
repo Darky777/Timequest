a842ed3ef4ac48625a090998a84fa3eed354382f
Сотников Иван <stn_iv@list.ru>
2018-09-15 15:26:55 +0300

    Added SDC

--- Issue_1/Issue_1.sv
+++ Issue_1/Issue_1.sv
@@ -1,1 +1,15 @@
-ываы
+module Issue_1 (
+    input        clk,
+    output logic led
+);
+
+logic [31:0 ] cnt;
+
+always_ff @(posedge clk) begin 
+    cnt <= cnt + 1'b1;
+    led <= cnt[31];
+end
+
+
+endmodule
+	
